d25f42483e4b8ddf401446971bbc09a266d65209
Matheu2b2t <johnatan.ruales@campusucc.edu.co>
2025-11-18 20:23:45 -0500

    first commit

---
+++ src/main/java/com/SICAE/SICAE_back/SicaeBackApplication.java
@@ -0,0 +1,13 @@
+package com.SICAE.SICAE_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicaeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicaeBackApplication.class, args);
+	}
+
+}
