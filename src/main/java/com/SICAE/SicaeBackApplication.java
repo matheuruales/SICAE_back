3d43d734e2dde734197c84e0c235df4f0e54ec6e
Matheu2b2t <johnatan.ruales@campusucc.edu.co>
2025-11-18 22:14:42 -0500

    code

--- src/main/java/com/SICAE/SicaeBackApplication.java
+++ src/main/java/com/SICAE/SicaeBackApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SicaeBackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SicaeBackApplication.class, args);
     }
